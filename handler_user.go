d9192db7e0d1a1f2121cf88eba1da5db2f41d5bc
elijah <cuiyeqing960904@gmail.com>
2023-10-29 15:11:40 -0400

    create user

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"rss/internal/database"
+	"time"
 
-func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
+	"github.com/google/uuid"
+)
 
+func (apiCfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
+	type parammeters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parammeters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("error parsing json"))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+		Name: params.Name,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("cannot create user"))
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
 }
