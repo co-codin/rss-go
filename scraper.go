eeb13ccff0829cda6042ef5f44b6a9e452fd88f3
elijah <cuiyeqing960904@gmail.com>
2023-10-30 11:54:01 -0400

    scraper structure

---
+++ scraper.go
@@ -0,0 +1,19 @@
+package main
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Language    string    `xml:"language"`
+		Item        []RSSItem `xml:"item"`
+	} `xmlm:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
